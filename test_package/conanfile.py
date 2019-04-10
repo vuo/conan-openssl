02dc9e3215da8380d6643c92cd061e480225b875
Steve Mokris <smokris@softpixel.com>
2019-04-10 18:56:38 -0400

    Replace qbs with cmake. [#15280]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,19 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 import platform
 
 class OpenSSLTestConan(ConanFile):
-    generators = 'qbs'
+    generators = 'cmake'
 
     requires = 'llvm/3.3-5@vuo/stable'
 
     def build(self):
-        self.run('qbs -f "%s"' % self.source_folder)
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def imports(self):
         self.copy('*', src='bin', dst='bin')
         self.copy('*', src='lib', dst='lib')
 
     def test(self):
-        self.run('qbs run -f "%s"' % self.source_folder)
+        self.run('./bin/test_package')
